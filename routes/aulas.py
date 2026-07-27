c773b49834717bea62305d4938129e07b29c5039
leomelgar <leo.melgar33@gmail.com>
2026-07-27 15:46:32 -0300

    gestion de aulas, completa

--- routes/aulas.py
+++ routes/aulas.py
@@ -10,8 +10,6 @@ def home():
     aulas = Aula.query.all()
     return render_template('/aulas/home.html', aulas=aulas)
 
-
-
 @aulas.route('/new', methods=["POST"])
 def new_aula():
     if request.method=="POST":
@@ -25,7 +23,29 @@ def new_aula():
         flash('Aula creada correctamente!')
         return redirect(url_for('aulas.home'))
 
-@aulas.route('/cursos/delete/<id_aula>')
+@aulas.route('/update/<id_aula>', methods=["POST"])
+def update_aula(id_aula):
+    aula = db.session.get(Aula, id_aula)
+    if not aula:
+            flash('Aula no existe.', 'error')
+            return redirect(url_for('aulas.home')) # Mejor usar redirect aquí
+    if request.method=="POST":
+        try:
+            aula.nombre_aula = request.form.get('nombre_aula') or aula.nombre_aula
+            aula.capacidad = request.form.get('capacidad') or aula.capacidad
+            aula.ubicacion = request.form.get('ubicacion') or aula.ubicacion
+            db.session.commit()
+            flash('¡Datos Actualizados con éxito!', 'success')
+            # Patrón PRG: Redirigir en lugar de renderizar directamente tras un POST
+            return redirect(url_for('aulas.home'))
+        except ValueError:
+                    db.session.rollback()
+                    flash("El formato de fecha ingresado no es válido.", "danger")
+                    return redirect(url_for('aulas.home'))
+            # Método GET: Mostrar el formulario con los datos actuales
+    return render_template("/aulas/home.html")
+
+@aulas.route('/aulas/delete/<id_aula>')
 def delete(id_aula):
     aula = Aula.query.get(id_aula)
     db.session.delete(aula)
